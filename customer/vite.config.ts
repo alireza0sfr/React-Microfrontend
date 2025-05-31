2da14ffb9db6cd7f9b6e1afb71124589187890e7
Alireza Safari <alireza.safaree@gmail.com>
2025-05-31 21:48:15 +0330

    feat(vite.config.ts): adds basic configurations for both host and customer

--- customer/vite.config.ts
+++ customer/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+  const CUSTOMER_PORT = Number(env.VITE_CUSTOMER_PORT) || 3001
+
+  return {
+    server: {
+      port: CUSTOMER_PORT,
+    },
+    preview: {
+      port: CUSTOMER_PORT,
+    },
+    plugins: [
+      react(),
+      federation({
+        name: "customer",
+        filename: "customer.js",
+        exposes: {
+          "./App": "./src/App.tsx",
+        },
+        shared: ["react"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+  const HOST_PORT = Number(env.VITE_HOST_PORT) || 3000
+  const CUSTOMER_PORT = Number(env.VITE_CUSTOMER_PORT) || 3001
+
+  return {
+    server: {
+      port: HOST_PORT,
+    },
+    preview: {
+      port: HOST_PORT,
+    },
+    plugins: [
+      react(),
+      federation({
+        name: 'host-app',
+        remotes: {
+          customer: `http://localhost:${CUSTOMER_PORT}/assets/customer.js`,
+        },
+        shared: ['react'],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
